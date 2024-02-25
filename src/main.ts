640a9f94cdf5ed5200445d246a2e004f37373ab4
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 14:00:39 +0300

    add: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 7077;
 
   app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Бэкенд для CRM-FLAT')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   await app.listen(PORT);
 }
