b9509f98141f5198d2aa761496ea6da757ff0f05
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 13:38:29 +0300

    add: createUser in users.service.ts (draft)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User } from './users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -11,8 +12,12 @@ export class UsersService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async createUser() {
-    return
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const user: User = this.userRepository.create(dto);
+
+    await this.userRepository.save(user);
+
+    return user;
   }
 
   async getAllUsers() {
