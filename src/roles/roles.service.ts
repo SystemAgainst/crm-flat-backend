62aa53898ad5bf52929a4309c192f94c9c3d951c
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 14:57:30 +0300

    add: added service and controller for role creation

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './roles.entity';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) {}
+
+  async createRole(dto: CreateRoleDto): Promise<Role> {
+    const user: Role = this.roleRepository.create(dto);
+
+    await this.roleRepository.save(user);
+
+    return user;
+  }
+
+  async getRoleByValue(value: string): Promise<Role | undefined> {
+    // TODO: realized getRoleByValue(value);
+    return undefined
+    // return await this.roleRepository.findOne({ where: { value } });
+  }
+}
