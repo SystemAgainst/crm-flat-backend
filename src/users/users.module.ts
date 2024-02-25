8f7ecd8584c534c920397fa065de4d1db4543805
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 14:43:37 +0300

    add: added opposite entity in role's module and users's module respectively and committed uncommitted files

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { Role } from '../roles/roles.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role])],
   controllers: [UsersController],
   providers: [UsersService],
 })
