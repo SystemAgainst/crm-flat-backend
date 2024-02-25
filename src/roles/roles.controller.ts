b711decb0d7e504f14a18583e7dbf978c5c3866e
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 15:09:41 +0300

    add: written documentation in swagger for Roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,18 +1,24 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Role } from './roles.entity';
 
+
+@ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @ApiOperation({summary: 'Создание роли'})
+  @ApiResponse({status: 200, type: Role})
   @Post()
   createRole(@Body() roleDto: CreateRoleDto) {
     return this.rolesService.createRole(roleDto);
   }
 
+  @ApiOperation({summary: 'Получение роли по значению'})
+  @ApiResponse({status: 200, type: [Role]})
   @Get('/:value')
   getUserByValue(@Param('value') value: string) {
     return this.rolesService.getRoleByValue(value);
