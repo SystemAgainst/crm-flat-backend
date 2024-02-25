b1d2d903fd29538cadfad84c970f82c794b917a4
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-25 13:12:32 +0300

    upd: added user entity and added user table to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.entity';
 
 @Module({
   imports: [
@@ -17,8 +18,9 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [],
+      entities: [User],
       synchronize: true,
+      autoLoadEntities: true,
     }),
     UsersModule,
   ],
