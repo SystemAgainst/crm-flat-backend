f4bc95e29d2aeb7eafa900f68910ee5e857d3986
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-02-27 17:47:02 +0300

    add: registered jwt-module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as process from 'process';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     UsersModule,
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY || 'secret',
+      signOptions: {
+        expiresIn: '24h',
+      }
+    })
   ]
 })
 export class AuthModule {}
